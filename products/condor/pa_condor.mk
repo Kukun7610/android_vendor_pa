777efc2a038fda68d9a10c365975996229674514
Kukun Mishra <kukun7610@gmail.com>
2017-09-29 22:26:15 +0530

    Update pa_condor.mk

--- products/condor/pa_condor.mk
+++ products/condor/pa_condor.mk
@@ -16,7 +16,7 @@
 ifeq (pa_condor,$(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
  
 # Inherit AOSP device configuration
 $(call inherit-product, device/motorola/condor/full_condor.mk)
